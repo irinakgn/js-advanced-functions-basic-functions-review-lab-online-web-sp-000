6bc8059a1bda7d0dfeec0dfcf5e2b8d90c7318dd
Irina Kagan <irinagamb@gmail.com>
2019-10-03 01:28:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ function saturdayFun(activity='roller-skate'){
 function mondayWork (activity='go to the office'){
   return `This Monday, I will ${activity}.`;
 }
+
+
+function wrapAdjective (){
+  function human(description='special'){
+    return `You are ${description}`;
+  } }
+
+var calculator = {
+  function add(){},
+  function subtract(){},
+  function multiply(){},
+  function divide(){}
+}
+
+function actionApplyer(){
+  
+}
+  
+  
+}
